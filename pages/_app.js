e84a7227284f3a898acd7a0baeacea2b058475b6
Rahil <rahilahmad1258@gmail.com>
2021-11-23 16:58:15 +0500

    Redux integration.

---
+++ pages/_app.js
@@ -0,0 +1,9 @@
+import "../styles/globals.css";
+import React from "react"
+import { wrapper } from "../redux/store"
+
+const MyApp = ({ Component, pageProps}) => (
+  <Component {...pageProps} />
+)
+
+export default wrapper.withRedux(MyApp);
