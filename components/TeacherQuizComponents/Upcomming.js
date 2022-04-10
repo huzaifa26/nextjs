df517eede4a476d1054841ab07193f285ad335a1
Abdullah <abdullah.javed9438>
2022-04-10 15:54:28 +0500

    responsive ungraded quiz section

--- components/TeacherQuizComponents/Upcomming.js
+++ components/TeacherQuizComponents/Upcomming.js
@@ -1,61 +1,67 @@
-import styles from './Upcomming.module.css'
+import styles from "./Upcomming.module.css";
 
 function Upcomming() {
+  const tableContent = [
+    {
+      qt: "Quiz Title",
+      c_g: "Class/Grade",
+      c: "Subject",
+      t_n: "Tutors Name",
+      sd_t: "20-10-2021 | 10:00 PM",
+      ed_t: "21-10-2021 | 10:00 PM",
+    },
+  ];
 
-    const tableContent=[
-        {
-            "qt":"Quiz Title",
-            "c_g":'Class/Grade',
-            "c":"Subject",
-            "t_n":'Tutors Name',
-            'sd_t':"20-10-2021 | 10:00 PM",
-            'ed_t':'21-10-2021 | 10:00 PM'
-        },
-    ]
-
-
-    return ( 
+  return (
     <div className={styles.upcomming}>
-        <div className={styles.heading}>
-            <div>
-                <h2>Upcoming Quizzes</h2>
-                <p>All published & Scheduled Quizzes List</p>
-            </div>
-            <div>
-                <button style={{backgroundImage:`url(filtericon.png)`}}>Filters</button>
-            </div>
+      <div className={styles.heading}>
+        <div>
+          <h2>Upcoming Quizzes</h2>
+          <p>All published & Scheduled Quizzes List</p>
         </div>
+        <div>
+          <button style={{ backgroundImage: `url(filtericon.png)` }}>
+            Filters
+          </button>
+        </div>
+      </div>
 
-        <table>
-                <tr className={styles.thead}>
-                    <div>
-                        <th>Quiz Title</th>
-                        <th>Subject</th>
-                        <th>Start Date & Time</th>
-                        <th>End Date & Time</th>
-                        <th>Actions</th>
-                    </div>
-                </tr>
+      <table>
+        <tr className={styles.thead}>
+          <div>
+            <th>Quiz Title</th>
+            <th>Subject</th>
+            <th>Start Date & Time</th>
+            <th>End Date & Time</th>
+            <th>Actions</th>
+          </div>
+        </tr>
 
-                {tableContent.map((c)=>{
-                    return(
-                        <tr key={''} className={styles.tbody}>
-                            <td>{c.qt}</td>
-                            <td>{c.c}</td>
-                            <td>{c.sd_t}</td>
-                            <td>{c.ed_t}</td>
-                            <td>
-                                <button><img alt="" src='./actionEye.png'/></button>
-                                <button><img alt="" src='./pen.png'/></button>
-                                <button><img alt="" src='./firstdraft.png'/></button>
-                                <button><img alt="" src='./chain.png'/></button>
-                            </td>
-                        </tr>
-                    )
-                })}
-        </table>
-    </div> 
-    );
+        {tableContent.map((c) => {
+          return (
+            <tr key={""} className={styles.tbody}>
+              <td>{c.qt}</td>
+              <td>{c.c}</td>
+              <td>{c.sd_t}</td>
+              <td>{c.ed_t}</td>
+              <td>
+                <button>
+                  <img alt="" src="./actionEye.png" />
+                </button>
+                <button>
+                  <img alt="" src="./pen.png" />
+                </button>
+
+                <button>
+                  <img alt="" src="./chain.png" />
+                </button>
+              </td>
+            </tr>
+          );
+        })}
+      </table>
+    </div>
+  );
 }
 
 export default Upcomming;
